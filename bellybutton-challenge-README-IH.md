3a995b354b40c0835f51c3f83d321e0750c9d6bf
iHashmi9 <ibsarhashmi786@gmail.com>
2023-11-29 15:11:30 -0500

    Final Commit

---
+++ bellybutton-challenge-README-IH.md
@@ -0,0 +1,10 @@
+Initiate page by selecting any other Test Subject ID No: other than 940
+Script does not populate for ID 940, however populates for the rest
+Script does not default populate - user has to select a sample ID other than 940
+
+Script mostly designed using ChatGPT and Plotly.js other than minor adjustments in layout and error message coding
+
+ln 26 - Reverse() function from chatgpt
+
+ln 37/60 - Margins adjustment from chatgpt
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,28 @@
-// Initializes the page with a default plot
-function init() {
-  // Assuming you have an initial sample ID (e.g., 940)
-  updatePlots(940);
-}
 
-/// Call updatePlots() when a change takes place to the DOM
 d3.selectAll("#selDataset").on("change", function () {
   let selectedSample = d3.select("#selDataset").property("value");
-  console.log(selectedSample);  // Move this line inside the event listener
-  updatePlots(selectedSample);
+  updatePlotly(selectedSample);
 });
 
-// This function is called when a dropdown menu item is selected
-function updatePlots(selectedSample) {
+// Define the optionChanged function
+function optionChanged(selectedSample) {
+  updatePlotly(selectedSample);
+}
+
+// Function to update the plots and metadata
+function updatePlotly(selectedSample) {
   // Fetch data from the specified URL
   d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(responseData => {
-    // Assuming responseData is an object with properties: names, metadata, samples
-    // If your data structure is different, adjust accordingly
+
+    // Update the dropdown menu
+    updateDropdown(responseData);
 
     // Extract the selected sample data
     let selectedData = responseData.samples.find(sample => sample.id === selectedSample);
 
     // Check if selectedData is defined before accessing its properties
     if (selectedData) {
-      // Update the bar chart
+      // Bar chart
       let barTrace = {
         x: selectedData.sample_values.slice(0, 10).reverse(),
         y: selectedData.otu_ids.slice(0, 10).reverse().map(id => `OTU ${id}`),
@@ -35,14 +34,12 @@ function updatePlots(selectedSample) {
       let barData = [barTrace];
 
       let barLayout = {
-        title: "Top 10 OTUs",
-        xaxis: { title: "Sample Values" },
-        yaxis: { title: "OTU ID" }
+        margin: { t: 50, b: 50, l: 100, r: 100 } // Adjusted margins for centering
       };
 
-      Plotly.newPlot("chart", barData, barLayout);
+      Plotly.newPlot("bar", barData, barLayout);
 
-      // Update the bubble chart
+      // Bubble chart
       let bubbleTrace = {
         x: selectedData.otu_ids,
         y: selectedData.sample_values,
@@ -58,10 +55,9 @@ function updatePlots(selectedSample) {
       let bubbleData = [bubbleTrace];
 
       let bubbleLayout = {
-        title: 'Bubble Chart for Each Sample',
         xaxis: { title: 'OTU IDs' },
-        yaxis: { title: 'Sample Values' },
-        showlegend: false
+        showlegend: false,
+        margin: { t: 50, b: 50, l: 100, r: 100 } // Adjusted margins for centering
       };
 
       Plotly.newPlot('bubble', bubbleData, bubbleLayout);
@@ -73,25 +69,30 @@ function updatePlots(selectedSample) {
       // Find the selected metadata
       let selectedMetadata = responseData.metadata.find(meta => meta.id === parseInt(selectedSample));
 
+      // Check if selectedMetadata is defined before accessing its properties
       if (selectedMetadata) {
         // Iterate through metadata and append key-value pairs to the HTML
         Object.entries(selectedMetadata).forEach(([key, value]) => {
           metadataDiv.append("p").text(`${key}: ${value}`);
         });
-
-        // Log information to the console
-        console.log("Metadata:", selectedMetadata);
-      } else {
-        metadataDiv.append("p").text("Metadata not available for the selected sample.");
-        console.warn("Metadata not found for the selected ID:", selectedSample);
       }
-    } else {
-      console.error("Sample data not found for the selected ID:", selectedSample);
     }
-  }).catch(error => {
-    console.error("Error fetching data:", error);
   });
 }
 
-// Call init() to initialize the page with a default plot
-//init();
+// Function to update the dropdown menu
+function updateDropdown(responseData) {
+  let dropdown = d3.select("#selDataset");
+
+  let dropdownOptions = responseData.names;
+
+  // Clear existing options
+  dropdown.html("");
+
+  dropdownOptions.forEach(option => {
+    dropdown.append("option").text(option).property("value", option);
+  });
+}
+
+// Call updatePlotly() to initialize the page with a default plot
+updatePlotly(940);
